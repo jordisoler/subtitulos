85a8c09fe33fa5ae562943889b28c85a9c154635
Jordi Soler <jordi.solerbusquets@gmail.com>
2016-01-30 12:43:04 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Mac OS found. Installing deps
+    sudo easy_install pip
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Linux platform found. Installing deps.
+    sudo apt-get install python-pip
+    sudo pip install --upgrade pip;
+else
+    echo "Installer supports debian and mac only."
+fi
+pip install patoolib
